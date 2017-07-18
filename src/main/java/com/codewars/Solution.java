6891b425593e7442dd7a8d7d1b15e445e30b6d12
SURAMERICANA\carlvarn <1037636590>
2017-07-18 13:50:50 -0500

    Solution

--- src/main/java/com/codewars/Solution.java
+++ src/main/java/com/codewars/Solution.java
@@ -5,6 +5,16 @@ package com.codewars;
  */
 public class Solution {
     public static String toCamelCase(String s) {
-        return "";
+        String response = "";
+        if(s.length() > 0){
+            String words[] = s.split("(-)|(_)");
+            response = words[0].substring(0,1) + (words[0].length() > 1 ? response+words[0].substring(1,words[0].length()).toLowerCase() : "");
+            for (int i = 1; i < words.length; i++) {
+                response = response + words[i].substring(0,1).toUpperCase() + words[i].substring(1,words[i].length()).toLowerCase();;
+            }
+            return response;
+        }else{
+            return response;
+        }
     }
 }
--- src/test/java/com/codewars/SolutionTest.java
+++ src/test/java/com/codewars/SolutionTest.java
@@ -6,7 +6,6 @@ package com.codewars;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class SolutionTest {
     @Test
@@ -20,5 +19,47 @@ public class SolutionTest {
         String input = "the-Stealth-Warrior";
         System.out.println("input: "+input);
         assertEquals("theStealthWarrior", Solution.toCamelCase(input));
+    }
+
+    @Test
+    public void testSomeUnderscoreUpperStart() {
+        String input = "The_Stealth_Warrior";
+        System.out.println("input: "+input);
+        assertEquals("TheStealthWarrior", Solution.toCamelCase(input));
+    }
+
+    @Test
+    public void testSomeDashUpperStart() {
+        String input = "The-stealth-warrior";
+        System.out.println("input: "+input);
+        assertEquals("TheStealthWarrior", Solution.toCamelCase(input));
+    }
+
+    @Test
+    public void testSomeUpperStart() {
+        String input = "The";
+        System.out.println("input: "+input);
+        assertEquals("The", Solution.toCamelCase(input));
+    }
+
+    @Test
+    public void testSomeLowerStart() {
+        String input = "the";
+        System.out.println("input: "+input);
+        assertEquals("the", Solution.toCamelCase(input));
+    }
+
+    @Test
+    public void testOneLetter() {
+        String input = "t";
+        System.out.println("input: "+input);
+        assertEquals("t", Solution.toCamelCase(input));
+    }
+
+    @Test
+    public void testEmpty() {
+        String input = "";
+        System.out.println("input: "+input);
+        assertEquals("", Solution.toCamelCase(input));
     }
 }
